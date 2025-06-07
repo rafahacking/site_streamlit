d4e5ca2c1a679f86e19f18d35493f07287947dcf
rafahacking <nunesrafa72@gmail.com>
2025-06-07 15:22:29 -0300

    Add files via upload

---
+++ analise2.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="Análise de Produtos",layout="wide")
+
+# print(dataframe)
+arquivo = st.file_uploader("Envie um arquivo Excel", type="xlsx")
+st.button("Enviar arquivo")
+
+if arquivo == None:
+    st.text("Suba o arquivo")
+else:
+
+    dataframe = pd.read_excel(arquivo)
+
+    maispesado = dataframe['Peso_Kg'].max()
+
+    quantidade_produtos = len(dataframe)
+
+    st.dataframe(dataframe)
+
+    colfilter1, colfilter2 = st.columns(2)
+
+    with colfilter1:
+        minimo = st.number_input(label="Mínimo", label_visibility='visible',min_value=0,max_value=int(maispesado))
+
+    with colfilter2:
+        maximo = st.number_input(label="Máximo", label_visibility="visible",min_value=0,max_value=int(maispesado))
+
+    filtrado = dataframe[(dataframe["Peso_Kg"] > minimo) & (dataframe["Peso_Kg"] < maximo) ]
+
+    total_kg = dataframe["Peso_Kg"].sum()
+
+    media_kg = dataframe["Peso_Kg"].mean()
+
+    produto_categoria = dataframe.groupby("Categoria")["ID_Produto"].count().reset_index()
+
+    st.title("Produtos em Estoque")
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total de Produtos: ", quantidade_produtos)
+
+    col2.metric("Total em KIlos: ", total_kg)
+
+    col3.metric("Media em Kilos: ", media_kg)
+
+    st.bar_chart(data=produto_categoria,x= "Categoria",y="ID_Produto")
